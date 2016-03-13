84e662f18a55e9f4eeb12bf68fb5c76045894203
robertwang <robertwang.tw@gmail.com>
2016-03-13 22:30:42 +0800

    Add article index page to list all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
 	def index
-		
+		@articles = Article.all
 	end
 
 	def new
@@ -18,7 +18,21 @@ class ArticlesController < ApplicationController
 		else
 			render :new
 		end
-		
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params_check)
+			flash[:notice] = "Article was successfully updated."
+			redirect_to article_path(@article)
+		else
+			render :edit
+		end
 	end
 
 	def show
