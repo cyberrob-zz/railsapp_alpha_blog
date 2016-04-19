11309654ca3c1b26fda38121e80b46b7c7be6d23
robertwang <robertwang.tw@gmail.com>
2016-04-19 23:21:12 +0800

    create users table & model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312143722) do
+ActiveRecord::Schema.define(version: 20160419145818) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 20160312143722) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
